554ccc81e144d5c9e095a029e8c33616907abb43
Zack Carlson <jzcarlson014@gmail.com>
2020-08-18 12:55:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,7 @@ def using_push(colors_in_the_rainbow, next_color)
   colors_in_the_rainbow.push next_color
 end
 
-def using_unshift(bourough_in_nyc, @new_neighborhood)
-  bourough_in_nyc.unshift(@new_neighborhood)
+def using_unshift(bourough_in_nyc, new_neighborhood)
+  bourough_in_nyc.unshift(new_neighborhood)
 end
 
